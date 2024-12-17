169f0c7d9b15e32431b9ddd795a0f3af70cf6db4
FINN-5 <bmd040510@gmail.com>
2024-12-17 22:43:45 +0700

    task-list

---
+++ task-list-app/app/Http/Controllers/TaskController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tasks = Task::all();
+        return view('tasks.index', compact('tasks'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('tasks.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+        $data = $request->all();
+        $data['completed'] = $request->has('completed') ? 1 : 0;
+        Task::create($data);
+
+        return redirect()->route('tasks.index')->with('success', 'Task created successfully.');
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $task = Task::findOrFail($id);
+        return view('tasks.show', compact('task'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $task = Task::findOrFail($id);
+        return view('tasks.edit', compact('task'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+        $task = Task::findOrFail($id);
+
+        // Xử lý giá trị của checkbox `completed`
+        $data = $request->all();
+        $data['completed'] = $request->has('completed') ? 1 : 0;
+        $task->update($data);
+
+        return redirect()->route('tasks.index')->with('success', 'Task updated successfully.');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $task = Task::findOrFail($id);
+        $task->delete();
+
+        return redirect()->route('tasks.index')->with('success', 'Task deleted successfully.');
+    }
+}
